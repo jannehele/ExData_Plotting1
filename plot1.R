3bd1c98fc6aee15e6f85eacb684344f28f1a33b2
jannehele <jannehele@gmail.com>
2015-10-10 16:45:49 +0300

    Update plot1.R
    final version

--- plot1.R
+++ plot1.R
@@ -1,9 +1,10 @@
 #
-# Read data
+# Read data, assumption is that data file is stored to working directory
 dat <- data.frame()
-dat <- read.table("./Ex_data_plotting1/household_power_consumption.txt",header=TRUE, sep= ";", na.strings ="?")
+dat <- read.table("./household_power_consumption.txt",header=TRUE, sep= ";", na.strings ="?")
+#
+#convert the date field format
 dat$Date <- as.Date(dat$Date, "%d/%m/%Y")
-dat$Time <- strptime(dat$Time,"%H:%M:%S")
 # take only two days from dataset
 dat1 <-subset(dat,(dat$Date == as.Date("2007/02/01")))
 dat2 <-subset(dat,(dat$Date == as.Date("2007/02/02")))
